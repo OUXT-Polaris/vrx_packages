0c00d46ec8cdac85a8488212f747a300a5f27490
Masaya Kataoka <ms.kataoka@gmail.com>
2019-05-09 23:16:58 +0900

    update rosinstall file

--- build_external.sh
+++ build_external.sh
@@ -16,6 +16,7 @@ cd build
 make -j $(nproc)
 sudo make install
 cd ../../
+# Install Nnabla Cuda Ext
 cd nnabla-ext-cuda
 pip install -U -r python/requirements.txt
 mkdir build
@@ -23,3 +24,4 @@ cd build
 ../../cmake-3.14.3-Linux-x86_64/bin/cmake -DNNABLA_DIR=../../nnabla -DCPPLIB_LIBRARY=../../nnabla/build/lib/libnnabla.so -DBUILD_PYTHON_PACKAGE=OFF -DBUILD_CPP_UTILS=ON ..
 make -j $(nproc)
 sudo make install
+cd ../../
